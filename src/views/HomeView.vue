d0c71632353c2aec4dd66b7e3e6263bf9ac9dfdd
Vue Play <author@vueplay.com>
2024-01-24 14:48:45 +0100

    Stored by Vue Play

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+	<Default></Default>
+</template>
+<script>
+	import Default from '@/components/templates/Default.vue';
+	export default {
+		components: {
+			Default: Default
+		},
+		data: () => ({})
+	};
+
+</script>
+<style scoped></style>
