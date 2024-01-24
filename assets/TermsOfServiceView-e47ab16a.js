3ac64256868589d75e00773bd7dbef48a8ab28da
steffanhalv <steffanhalv@users.noreply.github.com>
2024-01-24 14:26:45 +0000

    deploy: dcad4ac9bad046b7b8780407d488ea4b1eb845c3

--- assets/TermsOfServiceView-e47ab16a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c}from"./index-21921d99.js";const o={data:()=>({})};function s(t,a,n,i,_,f){return r(),c("div",null," Terms of service view ")}const d=e(o,[["render",s]]);export{d as default};
