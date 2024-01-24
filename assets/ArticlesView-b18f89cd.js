fb9c8b58742391f16c43c43b1f78eb7686c264a0
steffanhalv <steffanhalv@users.noreply.github.com>
2024-01-24 16:12:12 +0000

    deploy: f7599deb049a7b7dfe2b9658fc29f160c37726b7

---
+++ assets/ArticlesView-b18f89cd.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-3d2a11e4.js";const o={data:()=>({})};function r(s,a,n,i,_,l){return t(),c("div",null," Articles view ")}const d=e(o,[["render",r]]);export{d as default};
---
+++ assets/ContactView-2f9e1605.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as o}from"./index-3d2a11e4.js";const c={data:()=>({})};function a(n,r,s,_,p,i){return e(),o("div",null," Contact view ")}const f=t(c,[["render",a]]);export{f as default};
