03d09cf20d09ef097cd153fa146989282e9e314e
steffanhalv <steffanhalv@users.noreply.github.com>
2024-01-28 14:21:15 +0000

    deploy: 76d5f32079b774f26ab312da68ff734f46b10b3c

--- assets/ArticleView-9f0e3bd9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c}from"./index-3d2a11e4.js";const o={data:()=>({})};function r(a,n,s,i,_,l){return t(),c("div",null," Article view ")}const d=e(o,[["render",r]]);export{d as default};
---
+++ assets/TermsOfServiceView-742f101e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c}from"./index-8184be0d.js";const o={data:()=>({})};function s(t,a,n,i,_,f){return r(),c("div",null," Terms of service view ")}const d=e(o,[["render",s]]);export{d as default};
