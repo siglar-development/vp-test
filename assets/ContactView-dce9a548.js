42f4c25a55adb39aa0fdd624ba975f802104cb60
steffanhalv <steffanhalv@users.noreply.github.com>
2024-02-13 09:16:41 +0000

    deploy: c9e67880654dc3c671429a6e600792f378803802

--- assets/ContactView-dce9a548.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as o}from"./index-330ae556.js";const c={data:()=>({})};function a(n,r,s,_,p,i){return e(),o("div",null," Contact view ")}const f=t(c,[["render",a]]);export{f as default};
