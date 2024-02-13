9a2abef7530210f43f7d69c8541069f08c9e4a39
steffanhalv <steffanhalv@users.noreply.github.com>
2024-02-13 09:40:05 +0000

    deploy: 2671286a37c1b7a06a1fdb733ea383c2434adcbd

---
+++ assets/ArticleView-bbdfd5c5.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-e9393663.js";const o={data:()=>({})};function r(a,n,s,i,_,l){return t(),c("div",null," Article view ")}const d=e(o,[["render",r]]);export{d as default};
---
+++ assets/ArticlesView-c3eb7155.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-e9393663.js";const o={data:()=>({})};function r(s,a,n,i,_,l){return t(),c("div",null," Articles view ")}const d=e(o,[["render",r]]);export{d as default};
