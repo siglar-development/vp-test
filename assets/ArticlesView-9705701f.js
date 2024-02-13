8af08d870d8d4155a20ad06f7d9776f01782192f
steffanhalv <steffanhalv@users.noreply.github.com>
2024-02-13 09:13:01 +0000

    deploy: 61b26871a4008744b27c998b364a8d1f22f1dd28

--- assets/ArticlesView-9705701f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c}from"./index-fb3b5dfc.js";const o={data:()=>({})};function r(s,a,n,i,_,l){return t(),c("div",null," Articles view ")}const d=e(o,[["render",r]]);export{d as default};
--- assets/CategoryListView-00d5539e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o}from"./index-fb3b5dfc.js";const a={data:()=>({})};function r(s,c,n,i,_,p){return t(),o("div",null," Category list view ")}const d=e(a,[["render",r]]);export{d as default};
